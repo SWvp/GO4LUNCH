9c21b983802684f443f4928a3a2f20bb9b0b5af5
stephane warin <serkalidev@gmail.com>
2021-10-13 17:21:12 +0200

    update workmates fragment

--- app/src/main/java/com/kardabel/go4lunch/ui/workmates/WorkMatesFragment.java
+++ app/src/main/java/com/kardabel/go4lunch/ui/workmates/WorkMatesFragment.java
@@ -12,28 +12,28 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.kardabel.go4lunch.databinding.FragmentWorkmatesBinding;
+import com.kardabel.go4lunch.databinding.RecyclerviewWorkmatesBinding;
 
 public class WorkMatesFragment extends Fragment {
 
     private WorkMatesViewModel mWorkMatesViewModel;
-    private FragmentWorkmatesBinding binding;
+    private RecyclerviewWorkmatesBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         mWorkMatesViewModel =
                 new ViewModelProvider(this).get(WorkMatesViewModel.class);
 
-        binding = FragmentWorkmatesBinding.inflate(inflater, container, false);
+        binding = RecyclerviewWorkmatesBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textWorkmates;
-        mWorkMatesViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        //     final TextView textView = binding.textWorkmates;
+        //     mWorkMatesViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        //         @Override
+        //         public void onChanged(@Nullable String s) {
+        //             textView.setText(s);
+        //         }
+        //     });
         return root;
     }
 
