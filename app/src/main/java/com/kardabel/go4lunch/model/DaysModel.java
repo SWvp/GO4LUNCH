97601a79ca4450039e15cdcf2492ed91eb1c7ed5
stephane warin <serkalidev@gmail.com>
2021-11-09 18:11:19 +0100

    add pojo daysModel to set days collection

---
+++ app/src/main/java/com/kardabel/go4lunch/model/DaysModel.java
@@ -0,0 +1,21 @@
+package com.kardabel.go4lunch.model;
+
+public class DaysModel {
+
+    private String day;
+
+    public DaysModel() {
+    }
+
+    public DaysModel(String day) {
+        this.day = day;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+}
