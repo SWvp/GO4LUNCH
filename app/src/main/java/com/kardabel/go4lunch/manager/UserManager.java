e5089b5e7eed604c023e6e6959e5304b5b4a9988
stephane warin <serkalidev@gmail.com>
2021-11-12 14:04:06 +0100

    change var name

--- app/src/main/java/com/kardabel/go4lunch/manager/UserManager.java
+++ app/src/main/java/com/kardabel/go4lunch/manager/UserManager.java
@@ -5,15 +5,15 @@ import android.content.Context;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseUser;
 import com.kardabel.go4lunch.model.UserModel;
-import com.kardabel.go4lunch.repository.UserRepository;
+import com.kardabel.go4lunch.usecase.FirestoreUseCase;
 
 public class UserManager {
 
     private static volatile UserManager instance;
-    private UserRepository userRepository;
+    private FirestoreUseCase mFirestoreUseCase;
 
     private UserManager() {
-        userRepository = UserRepository.getInstance();
+        mFirestoreUseCase = FirestoreUseCase.getInstance();
     }
 
     public static UserManager getInstance() {
@@ -21,7 +21,7 @@ public class UserManager {
         if (result != null) {
             return result;
         }
-        synchronized(UserRepository.class) {
+        synchronized(FirestoreUseCase.class) {
             if (instance == null) {
                 instance = new UserManager();
             }
@@ -30,23 +30,23 @@ public class UserManager {
     }
 
     public Boolean isCurrentUserLogged(){
-        return (userRepository.getCurrentUser() != null);
+        return (mFirestoreUseCase.getCurrentUser() != null);
     }
 
     public void createUser() {
-        userRepository.createUser();
+        mFirestoreUseCase.createUser();
     }
 
     public FirebaseUser getCurrentUser(){
-        return userRepository.getCurrentUser();
+        return mFirestoreUseCase.getCurrentUser();
     }
 
     public Task<Void> signOut(Context context){
-        return userRepository.signOut(context);
+        return mFirestoreUseCase.signOut(context);
     }
 
     public Task<UserModel> getUserData() {
         // Get the user from Firestore and cast it to a User model Object
-        return userRepository.getUserData().continueWith(task -> task.getResult().toObject(UserModel.class));
+        return mFirestoreUseCase.getUserData().continueWith(task -> task.getResult().toObject(UserModel.class));
     }
 }
