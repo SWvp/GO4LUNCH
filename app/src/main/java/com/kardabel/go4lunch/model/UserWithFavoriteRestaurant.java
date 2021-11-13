5da70f52ceccca7f087343f3c36794d25657bd9c
stephane warin <serkalidev@gmail.com>
2021-11-13 10:39:24 +0100

    Pojo : document with fields needed by Firestore

---
+++ app/src/main/java/com/kardabel/go4lunch/model/UserWithFavoriteRestaurant.java
@@ -0,0 +1,41 @@
+package com.kardabel.go4lunch.model;
+
+public class UserWithFavoriteRestaurant {
+
+    private String restaurantId;
+    private String restaurantName;
+    private String userId;
+
+    public UserWithFavoriteRestaurant() {
+    }
+
+    public UserWithFavoriteRestaurant(String restaurantId, String restaurantName, String userId) {
+        this.restaurantId = restaurantId;
+        this.restaurantName = restaurantName;
+        this.userId = userId;
+    }
+
+    public String getRestaurantId() {
+        return restaurantId;
+    }
+
+    public void setRestaurantId(String restaurantId) {
+        this.restaurantId = restaurantId;
+    }
+
+    public String getRestaurantName() {
+        return restaurantName;
+    }
+
+    public void setRestaurantName(String restaurantName) {
+        this.restaurantName = restaurantName;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
