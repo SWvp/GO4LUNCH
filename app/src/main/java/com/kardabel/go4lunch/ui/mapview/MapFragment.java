68d109b122e6571c3f6afb5c493c59cd3eb21537
stephane warin <serkalidev@gmail.com>
2021-11-16 15:29:17 +0100

    map can now check what is the favorite user restaurant

--- app/src/main/java/com/kardabel/go4lunch/ui/mapview/MapFragment.java
+++ app/src/main/java/com/kardabel/go4lunch/ui/mapview/MapFragment.java
@@ -1,13 +1,11 @@
 package com.kardabel.go4lunch.ui.mapview;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -16,7 +14,6 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
-import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.kardabel.go4lunch.R;
@@ -27,14 +24,10 @@ import com.kardabel.go4lunch.util.SvgToBitmapConverter;
 
 public class MapFragment extends SupportMapFragment implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
-    private MapViewModel mMapViewModel;
-    private GoogleMap googleMap;
-
     public MapFragment()  { getMapAsync(this); }
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
-        this.googleMap = googleMap;
 
         // CUSTOM MAP WITHOUT POI WE DON'T NEED
         googleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
@@ -46,35 +39,44 @@ public class MapFragment extends SupportMapFragment implements OnMapReadyCallbac
 
             // CONFIGURE MAPVIEWMODEL
             ViewModelFactory listViewModelFactory = ViewModelFactory.getInstance();
-            mMapViewModel =
-                    new ViewModelProvider(this, listViewModelFactory).get(MapViewModel.class);
+            MapViewModel mapViewModel = new ViewModelProvider(this, listViewModelFactory).get(MapViewModel.class);
 
-            mMapViewModel.getMapViewStateLiveData().observe(this, new Observer<MapViewState>() {
-                @SuppressLint("MissingPermission")
-                @Override
-                public void onChanged(MapViewState mapViewState) {
+            mapViewModel.getMapViewStateLiveData().observe(this, mapViewState -> {
 
-                    // MOVE THE CAMERA TO THE USER LOCATION
-                    googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mapViewState.getLatLng(), mapViewState.getZoom()));
+                // MOVE THE CAMERA TO THE USER LOCATION
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mapViewState.getLatLng(), mapViewState.getZoom()));
 
-                    // DISPLAY BLUE DOT FOR USER LOCATION
-                    googleMap.setMyLocationEnabled(true);
+                // DISPLAY BLUE DOT FOR USER LOCATION
+                googleMap.setMyLocationEnabled(true);
 
-                    // ZOOM IN, ANIMATE CAMERA
-                    googleMap.animateCamera(CameraUpdateFactory.zoomIn());
+                // ZOOM IN, ANIMATE CAMERA
+                googleMap.animateCamera(CameraUpdateFactory.zoomIn());
 
-                    // CAMERA POSITION
-                    CameraPosition cameraPosition = new CameraPosition.Builder()
-                            .target(mapViewState.getLatLng())      // Sets the center of the map to Mountain View
-                            .zoom(17)                   // Sets the zoom
-                            .bearing(90)                // Sets the orientation of the camera to east
-                            .tilt(30)                   // Sets the tilt of the camera to 30 degrees
-                            .build();                   // Creates a CameraPosition from the builder
-                    googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+                // CAMERA POSITION
+                CameraPosition cameraPosition = new CameraPosition.Builder()
+                        .target(mapViewState.getLatLng())      // Sets the center of the map to Mountain View
+                        .zoom(17)                   // Sets the zoom
+                        .bearing(90)                // Sets the orientation of the camera to east
+                        .tilt(30)                   // Sets the tilt of the camera to 30 degrees
+                        .build();                   // Creates a CameraPosition from the builder
+                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
 
-                    // SET EVERY POI ON THE MAP
-                    for (Poi poi : mapViewState.getPoiList()) {
-                        Marker marker = googleMap.addMarker(new MarkerOptions()
+                // SET EVERY POI ON THE MAP
+                for (Poi poi : mapViewState.getPoiList()) {
+                    Marker marker;
+                    if(poi.getIsFavorite()){
+                        marker = googleMap.addMarker(new MarkerOptions()
+                                .position(poi.getPoiLatLng())
+                                .title(poi.getPoiName())
+                                .snippet(poi.getPoiAddress())
+                                .icon(BitmapDescriptorFactory
+                                        .fromBitmap(SvgToBitmapConverter
+                                                .getBitmapFromVectorDrawable(requireContext(),
+                                                        R.drawable.restaurant_poi_icon_green))));
+                        // SET TAG TO RETRIEVE THE MARKER IN onMarkerClick METHOD
+
+                    }else{
+                        marker = googleMap.addMarker(new MarkerOptions()
                                 .position(poi.getPoiLatLng())
                                 .title(poi.getPoiName())
                                 .snippet(poi.getPoiAddress())
@@ -82,15 +84,16 @@ public class MapFragment extends SupportMapFragment implements OnMapReadyCallbac
                                         .fromBitmap(SvgToBitmapConverter
                                                 .getBitmapFromVectorDrawable(requireContext(),
                                                         R.drawable.restaurant_poi_icon_red))));
-
                         // SET TAG TO RETRIEVE THE MARKER IN onMarkerClick METHOD
-                        marker.setTag(poi.getPoiPlaceId());
+
 
                     }
-                    Log.d("pipo", "onChanged() called with: mapViewViewState = [" + mapViewState + "]");
+                    assert marker != null;
+                    marker.setTag(poi.getPoiPlaceId());
                 }
-            });
+                Log.d("pipo", "onChanged() called with: mapViewViewState = [" + mapViewState + "]");
 
+            });
             // SET A LISTENER FOR MARKER CLICK
             googleMap.setOnMarkerClickListener(this);
 
--- app/src/main/java/com/kardabel/go4lunch/ui/mapview/MapViewModel.java
+++ app/src/main/java/com/kardabel/go4lunch/ui/mapview/MapViewModel.java
@@ -6,11 +6,14 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MediatorLiveData;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModel;
 
 import com.google.android.gms.maps.model.LatLng;
+import com.kardabel.go4lunch.model.UserWithFavoriteRestaurant;
 import com.kardabel.go4lunch.pojo.NearbySearchResults;
 import com.kardabel.go4lunch.repository.LocationRepository;
+import com.kardabel.go4lunch.repository.WorkmatesRepository;
 import com.kardabel.go4lunch.usecase.NearbySearchResultsUseCase;
 
 import java.util.ArrayList;
@@ -20,49 +23,92 @@ public class MapViewModel extends ViewModel {
 
     public static final float ZOOM_FOCUS = 15F;
 
-    private final MediatorLiveData<MapViewState> mapViewStatePoiLiveData = new MediatorLiveData<>();
+    private final MediatorLiveData<MapViewState> mapViewStatePoiMediatorLiveData = new MediatorLiveData<>();
 
-    public MapViewModel(@NonNull LocationRepository locationRepository, @NonNull NearbySearchResultsUseCase nearbySearchResultsUseCase) {
+    public MapViewModel(@NonNull LocationRepository locationRepository,
+                        @NonNull NearbySearchResultsUseCase nearbySearchResultsUseCase,
+                        @NonNull WorkmatesRepository workmatesRepository) {
 
         LiveData<Location> locationLiveData = locationRepository.getLocationLiveData();
         LiveData<NearbySearchResults> nearbySearchResultsLiveData = nearbySearchResultsUseCase.getNearbySearchResultsLiveData();
+        LiveData<List<UserWithFavoriteRestaurant>> favoriteRestaurantsLiveData = workmatesRepository.getRestaurantsAddAsFavorite();
 
-        mapViewStatePoiLiveData.addSource(locationLiveData, location -> combine(nearbySearchResultsLiveData.getValue(), location));
-        mapViewStatePoiLiveData.addSource(nearbySearchResultsLiveData, nearbySearchResults -> combine(nearbySearchResults, locationLiveData.getValue()));
+        // OBSERVERS
+
+        mapViewStatePoiMediatorLiveData.addSource(locationLiveData, location -> MapViewModel.this.combine(
+                nearbySearchResultsLiveData.getValue(),
+                location,
+                favoriteRestaurantsLiveData.getValue()));
+        mapViewStatePoiMediatorLiveData.addSource(nearbySearchResultsLiveData, nearbySearchResults -> combine(
+                nearbySearchResults,
+                locationLiveData.getValue(),
+                favoriteRestaurantsLiveData.getValue()));
+        mapViewStatePoiMediatorLiveData.addSource(favoriteRestaurantsLiveData, userWithFavoriteRestaurants -> combine(
+                nearbySearchResultsLiveData.getValue(),
+                locationLiveData.getValue(),
+                userWithFavoriteRestaurants));
 
     }
 
-    private void combine(@Nullable NearbySearchResults nearbySearchResults, @Nullable Location location) {
-        if (nearbySearchResults == null || location == null) {
-            return;
+    private void combine(@Nullable NearbySearchResults nearbySearchResults,
+                         @Nullable Location location,
+                         @Nullable List<UserWithFavoriteRestaurant> userWithFavoriteRestaurant) {
+        if (nearbySearchResults != null && location != null) {
+            mapViewStatePoiMediatorLiveData.setValue(map(nearbySearchResults, location, userWithFavoriteRestaurant));
         }
+
+
+    }
+
+    private MapViewState map(NearbySearchResults nearbySearchResults, Location location, List<UserWithFavoriteRestaurant> userWithFavoriteRestaurant){
         List<Poi> poiList = new ArrayList<>();
+        List<String> restaurantAsFavoriteId = new ArrayList<>();
+
+        if (userWithFavoriteRestaurant != null) {
+            for (int i = 0; i < userWithFavoriteRestaurant.size(); i++) {
+                restaurantAsFavoriteId.add(userWithFavoriteRestaurant.get(i).getRestaurantId());
+
+            }
+        }
 
         for (int i = 0; i < nearbySearchResults.getResults().size(); i++) {
+            boolean isFavorite = false;
             String poiName = nearbySearchResults.getResults().get(i).getRestaurantName();
             String poiPlaceId = nearbySearchResults.getResults().get(i).getRestaurantId();
             String poiAddress = nearbySearchResults.getResults().get(i).getRestaurantAddress();
             LatLng latLng = new LatLng(nearbySearchResults.getResults().get(i).getRestaurantGeometry().getRestaurantLatLngLiteral().getLat(),
                     nearbySearchResults.getResults().get(i).getRestaurantGeometry().getRestaurantLatLngLiteral().getLng());
+            if(userWithFavoriteRestaurant != null && restaurantAsFavoriteId.contains(poiPlaceId)){
+                isFavorite = true;
+            }
 
             poiList.add(
                     new Poi(
                             poiName,
                             poiPlaceId,
                             poiAddress,
-                            latLng
+                            latLng,
+                            isFavorite
                     )
             );
 
         }
 
-        LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
+        LatLng userLocation = new LatLng(
+                location.getLatitude(),
+                location.getLongitude());
 
-        mapViewStatePoiLiveData.setValue(new MapViewState(poiList, new LatLng(userLocation.latitude, userLocation.longitude), ZOOM_FOCUS));
+        return new MapViewState(
+                poiList,
+                new LatLng(
+                        userLocation.latitude,
+                        userLocation.longitude),
+                ZOOM_FOCUS);
+
     }
 
     // LIVEDATA OBSERVED BY MAP FRAGMENT
     public LiveData<MapViewState> getMapViewStateLiveData() {
-        return mapViewStatePoiLiveData;
+        return mapViewStatePoiMediatorLiveData;
     }
 }
