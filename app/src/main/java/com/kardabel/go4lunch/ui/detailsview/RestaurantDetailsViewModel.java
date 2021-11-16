a7db9e5c8612f0ce7899e3f93ba5897409408d73
stephane warin <serkalidev@gmail.com>
2021-11-16 15:37:27 +0100

    upgrade details VM with rating and fav

--- app/src/main/java/com/kardabel/go4lunch/ui/detailsview/RestaurantDetailsViewModel.java
+++ app/src/main/java/com/kardabel/go4lunch/ui/detailsview/RestaurantDetailsViewModel.java
@@ -2,55 +2,168 @@ package com.kardabel.go4lunch.ui.detailsview;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.arch.core.util.Function;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MediatorLiveData;
-import androidx.lifecycle.Transformations;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModel;
 
 import com.kardabel.go4lunch.pojo.PlaceSearchResults;
 import com.kardabel.go4lunch.pojo.Photo;
 import com.kardabel.go4lunch.usecase.PlaceSearchResultsUseCase;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RestaurantDetailsViewModel extends ViewModel {
 
     private String placeId = "";
-
-    private final LiveData<RestaurantDetailsViewState> restaurantDetailsViewStateLiveData;
-    private final MediatorLiveData<List<WorkmatesDetailsViewState>> workMatesDetailsViewStateMediatorLiveData = new MediatorLiveData<>();
+    private final MediatorLiveData<RestaurantDetailsViewState> workMatesDetailsViewStateMediatorLiveData = new MediatorLiveData<>();
+    private final MediatorLiveData<List<DetailsWorkmatesViewState>> workmatesLikeThisRestaurantMediatorLiveData = new MediatorLiveData<>();
     private RestaurantDetailsViewState result;
     private final FirestoreUseCase firestoreUseCase;
 
+
     public RestaurantDetailsViewModel(@Nullable PlaceSearchResultsUseCase placeSearchResultsUseCase){
 
-        // UPDATE LIVEDATA WITH MAP FUNCTION
-        //listViewViewStateLiveData = Transformations.map(nearbyResultsLiveData, input -> map(input));
-        restaurantDetailsViewStateLiveData = Transformations.map(placeSearchResultsUseCase.getPlaceSearchResultsLiveData(), new Function<PlaceSearchResults, RestaurantDetailsViewState>() {
+        // OBSERVERS
+
+        workMatesDetailsViewStateMediatorLiveData.addSource(nearbySearchResultsLiveData, nearbySearchResults -> combine(
+                nearbySearchResults,
+                userWithFavoriteRestaurantLiveData.getValue(),
+                restaurantDetailsResultsUseCaseLiveData.getValue()));
+
+        workMatesDetailsViewStateMediatorLiveData.addSource(userWithFavoriteRestaurantLiveData, userWithFavoriteRestaurants -> combine(
+                nearbySearchResultsLiveData.getValue(),
+                userWithFavoriteRestaurants,
+                restaurantDetailsResultsUseCaseLiveData.getValue()));
+
+        workMatesDetailsViewStateMediatorLiveData.addSource(restaurantDetailsResultsUseCaseLiveData, restaurantDetailsResults -> combine(
+                nearbySearchResultsLiveData.getValue(),
+                userWithFavoriteRestaurantLiveData.getValue(),
+                restaurantDetailsResults));
+
+
+        // OBSERVERS FOR WORKMATES RECYCLERVIEW
+
+        workmatesLikeThisRestaurantMediatorLiveData.addSource(userWithFavoriteRestaurantLiveData, new Observer<List<UserWithFavoriteRestaurant>>() {
             @Override
-            public RestaurantDetailsViewState apply(PlaceSearchResults input) {
-                return RestaurantDetailsViewModel.this.map(input);
+            public void onChanged(List<UserWithFavoriteRestaurant> userWithFavoriteRestaurants) {
+                mapWorkmates(userWithFavoriteRestaurants, workMatesLiveData.getValue());
+
             }
         });
 
+        workmatesLikeThisRestaurantMediatorLiveData.addSource(workMatesLiveData, new Observer<List<UserModel>>() {
+            @Override
+            public void onChanged(List<UserModel> userModels) {
+                mapWorkmates(userWithFavoriteRestaurantLiveData.getValue(), userModels);
 
-
-
+            }
+        });
     }
 
-    private RestaurantDetailsViewState map(@NonNull PlaceSearchResults placeSearchResults) {
+    private void mapWorkmates(List<UserWithFavoriteRestaurant> userWithFavoriteRestaurants, List<UserModel> users) {
+        if (userWithFavoriteRestaurants != null && users != null) {
+            List<DetailsWorkmatesViewState> workMatesViewStateList = new ArrayList<>();
+            for (int i = 0; i < userWithFavoriteRestaurants.size(); i++) {
+                if (userWithFavoriteRestaurants.get(i).getRestaurantId().equals(placeId)) {
+                    for (int j = 0; j < users.size(); j++) {
+                        if (users.get(j).getUid().equals(userWithFavoriteRestaurants.get(i).getUserId())) {
+                            String name = users.get(j).getUserName();
+                            String avatar = users.get(j).getAvatarURL();
+
+                            workMatesViewStateList.add(new DetailsWorkmatesViewState(
+                                    name,
+                                    avatar
+                            ));
+
+                        }
+                    }
+                }
+            }
+            workmatesLikeThisRestaurantMediatorLiveData.setValue(workMatesViewStateList);
+        }
+    }
+
+
+    private void combine(@Nullable NearbySearchResults nearbySearchResults,
+                         @Nullable List<UserWithFavoriteRestaurant> usersWithFavoriteRestaurant,
+                         @Nullable List<RestaurantDetailsResult> restaurantDetailsResults) {
+        if (nearbySearchResults != null && restaurantDetailsResults == null) {
+            workMatesDetailsViewStateMediatorLiveData.setValue(mapWithoutDetails(nearbySearchResults, usersWithFavoriteRestaurant));
+        }else if (nearbySearchResults != null){
+            workMatesDetailsViewStateMediatorLiveData.setValue(map(nearbySearchResults,restaurantDetailsResults ,usersWithFavoriteRestaurant));
+        }
+    }
+
+    // MAP WITHOUT DETAILS, IT CAN HAPPEN WHEN USER HAS NO NETWORK AVAILABLE
+    // (ASSUME THAT THE GOOGLE DETAILS SERVICE WILL WORK AS LONG AS NEARBY WORKS)
+    private RestaurantDetailsViewState mapWithoutDetails(@NonNull NearbySearchResults nearbySearchResults,
+                                                         List<UserWithFavoriteRestaurant> usersWithFavoriteRestaurant) {
 
         for (int i = 0; i < placeSearchResults.getResults().size(); i++) {
             if(placeId.equals(placeSearchResults.getResults().get(i).getPlaceId())){
 
                 result = new RestaurantDetailsViewState(
-                        placeSearchResults.getResults().get(i).getRestaurantName(),
-                        placeSearchResults.getResults().get(i).getRestaurantAddress(),
-                        photoReference(placeSearchResults.getResults().get(i).getRestaurantPhotos()),
-                        "23 48 23 48",
-                        "www.ouioui.com",
-                        nearbySearchResults.getResults().get(i).getRestaurantId()
+                        nearbySearchResults.getResults().get(i).getRestaurantName(),
+                        nearbySearchResults.getResults().get(i).getRestaurantAddress(),
+                        photoReference(nearbySearchResults.getResults().get(i).getRestaurantPhotos()),
+                        "",
+                        "",
+                        nearbySearchResults.getResults().get(i).getRestaurantId(),
+                        convertRatingStars(nearbySearchResults.getResults().get(i).getRating()),
+                        isFavorite
+
+                );
+            }
+        }
+        return result;
+
+    }
+
+    private RestaurantDetailsViewState map(@NonNull NearbySearchResults nearbySearchResults,
+                                           @NonNull List<RestaurantDetailsResult> restaurantDetailsResults,
+                                           List<UserWithFavoriteRestaurant> usersWithFavoriteRestaurant) {
+
+        List<String> restaurantAsFavoriteId = new ArrayList<>();
+        String restaurantPhoneNumber = "";
+        String restaurantWebsite = "";
+
+        if (usersWithFavoriteRestaurant != null) {
+            for (int i = 0; i < usersWithFavoriteRestaurant.size(); i++) {
+                restaurantAsFavoriteId.add(usersWithFavoriteRestaurant.get(i).getRestaurantId());
+
+            }
+        }
+
+        for (int i = 0; i < restaurantDetailsResults.size(); i++) {
+            if (restaurantDetailsResults.get(i).getDetailsResult().getPlaceId().equals(placeId)) {
+                if (restaurantDetailsResults.get(i).getDetailsResult().getFormatted_phone_number() != null) {
+                    restaurantPhoneNumber = restaurantDetailsResults.get(i).getDetailsResult().getFormatted_phone_number();
+
+                }
+                if (restaurantDetailsResults.get(i).getDetailsResult().getWebsite() != null) {
+                    restaurantWebsite = restaurantDetailsResults.get(i).getDetailsResult().getWebsite();;
+
+                }
+            }
+        }
+
+        for (int i = 0; i < nearbySearchResults.getResults().size(); i++) {
+            if (placeId.equals(nearbySearchResults.getResults().get(i).getRestaurantId())) {
+                boolean isFavorite = false;
+                if(restaurantAsFavoriteId.contains(nearbySearchResults.getResults().get(i).getRestaurantId())){
+                    isFavorite = true;
+                }
+                result = new RestaurantDetailsViewState(
+                        nearbySearchResults.getResults().get(i).getRestaurantName(),
+                        nearbySearchResults.getResults().get(i).getRestaurantAddress(),
+                        photoReference(nearbySearchResults.getResults().get(i).getRestaurantPhotos()),
+                        restaurantPhoneNumber,
+                        restaurantWebsite,
+                        nearbySearchResults.getResults().get(i).getRestaurantId(),
+                        convertRatingStars(nearbySearchResults.getResults().get(i).getRating()),
+                        isFavorite
 
                 );
             }
@@ -60,10 +173,10 @@ public class RestaurantDetailsViewModel extends ViewModel {
     }
 
     // SEARCH FOR A PHOTO IN THE LIST PROVIDED BY NEARBY PLACES
-    private String photoReference(List<Photo> photoList){
-        if (photoList != null){
-            for (Photo photo : photoList){
-                if (!photo.getPhotoReference().isEmpty()){
+    private String photoReference(List<Photo> photoList) {
+        if (photoList != null) {
+            for (Photo photo : photoList) {
+                if (!photo.getPhotoReference().isEmpty()) {
                     return photo.getPhotoReference();
 
                 }
@@ -73,24 +186,30 @@ public class RestaurantDetailsViewModel extends ViewModel {
 
     }
 
+    private double convertRatingStars(double rating) {
+        // GIVE AN INTEGER (NUMBER ROUNDED TO THE NEAREST INTEGER)
+        return Math.round(rating * 3 / 5);
+
+    }
+
+    // GET RESTAURANT TO PARSE HIS DETAILS
     public void init(String placeId){
         this.placeId = placeId;
     }
 
+    // SAY TO FIRESTORE THIS RESTAURANT IS ON FAVORITE OR NOT
     public void onFavoriteClick(String restaurantId, String restaurantName){
         firestoreUseCase.onFavoriteClick(restaurantId, restaurantName);
 
     }
 
     public LiveData<RestaurantDetailsViewState> getRestaurantDetailsViewStateLiveData(){
-        return restaurantDetailsViewStateLiveData;
-
-    }
-
-    public LiveData<List<WorkmatesDetailsViewState>> getDetailsWorkmatesViewStateLiveData() {
         return workMatesDetailsViewStateMediatorLiveData;
 
     }
 
+    public LiveData<List<DetailsWorkmatesViewState>> getWorkmatesLikeThisRestaurant() {
+        return workmatesLikeThisRestaurantMediatorLiveData;
 
+    }
 }
