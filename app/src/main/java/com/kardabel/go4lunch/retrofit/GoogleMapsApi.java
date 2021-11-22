78f3fc87b2dfef6e994a4f99e1a3682387090050
stephane warin <serkalidev@gmail.com>
2021-11-22 09:56:20 +0100

    update googleMapApi with new query

--- app/src/main/java/com/kardabel/go4lunch/retrofit/GoogleMapsApi.java
+++ app/src/main/java/com/kardabel/go4lunch/retrofit/GoogleMapsApi.java
@@ -1,7 +1,9 @@
 package com.kardabel.go4lunch.retrofit;
 
+import com.kardabel.go4lunch.pojo.AutocompleteResponse;
 import com.kardabel.go4lunch.pojo.RestaurantDetailsResult;
 import com.kardabel.go4lunch.pojo.NearbySearchResults;
+import com.kardabel.go4lunch.pojo.SearchViewResult;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
@@ -24,13 +26,26 @@ public interface GoogleMapsApi {
             @Query("fields") String fields
     );
 
-    @GET("maps/api/place/nearbysearch/json")
-    Call<NearbySearchResults> searchRestaurantWithSearchView(
+    // Todo: make a better google price with more defined fields
+    @GET("maps/api/place/details/json")
+    Call<SearchViewResult> searchViewResult(
+            @Query("key") String key,
+            @Query("place_id") String place_id
+    );
+
+
+
+
+
+
+
+    @GET("maps/api/place/autocomplete/json")
+    Call<AutocompleteResponse> autocompleteResult(
             @Query("key") String key,
             @Query("type") String type,
             @Query("location") String location,
             @Query("radius") String radius,
-            @Query("keyword") String keyword
+            @Query("input") String input
     );
 
 
