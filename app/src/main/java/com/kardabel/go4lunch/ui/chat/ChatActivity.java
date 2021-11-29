2a105fff790cc9a6a5016b38b707c2dcd948e94d
stephane warin <serkalidev@gmail.com>
2021-11-29 11:13:36 +0100

    adapter for chat activity

--- app/src/main/java/com/kardabel/go4lunch/ui/chat/ChatActivity.java
+++ app/src/main/java/com/kardabel/go4lunch/ui/chat/ChatActivity.java
@@ -3,22 +3,37 @@ package com.kardabel.go4lunch.ui.chat;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.kardabel.go4lunch.MainActivity;
 import com.kardabel.go4lunch.databinding.ChatActivityBinding;
+import com.kardabel.go4lunch.di.ViewModelFactory;
+import com.kardabel.go4lunch.ui.detailsview.DetailsRecyclerViewAdapter;
 import com.kardabel.go4lunch.ui.detailsview.RestaurantDetailsActivity;
+import com.kardabel.go4lunch.ui.detailsview.RestaurantDetailsViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class ChatActivity extends AppCompatActivity {
 
-    private static final String WORMATE_ID = "WORKMATE_ID";
+    private static final String WORKMATE_ID = "WORKMATE_ID";
 
     private ChatActivityBinding binding;
+    private ChatViewModel chatViewModel;
 
-    public static Intent navigate(Context context, String workmateId){
+    public static Intent navigate(Context context, String workmateId) {
         Intent intent = new Intent(context, ChatActivity.class);
-        intent.putExtra(WORMATE_ID, workmateId);
+        intent.putExtra(WORKMATE_ID, workmateId);
         return intent;
     }
 
@@ -27,5 +42,50 @@ public class ChatActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ChatActivityBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        List<ChatViewState> chatMessageList = new ArrayList<>();
+        ChatAdapter adapter = new ChatAdapter(this);
+
+        binding.activityChatRecyclerView.setAdapter(adapter);
+        binding.activityChatRecyclerView.setLayoutManager(
+                new LinearLayoutManager(
+                        ChatActivity.this,
+                        RecyclerView.VERTICAL,
+                        false));
+
+        // CONFIGURE VIEWMODEL
+        ViewModelFactory chatViewModelFactory = ViewModelFactory.getInstance();
+        chatViewModel =
+                new ViewModelProvider(this, chatViewModelFactory).get(ChatViewModel.class);
+
+        // INIT THE VIEW MODEL WITH THE WORKMATE ID PASSED IN INTENT
+        // (WE NEED USER ONE TO BUT IT WILL BE LATER)
+        Intent intent = getIntent();
+        chatViewModel.init(intent.getStringExtra(WORKMATE_ID));
+
+        chatViewModel.getChatMessages().observe(this, new Observer<List<ChatViewState>>() {
+            @Override
+            public void onChanged(List<ChatViewState> chatViewStatesList) {
+                adapter.setChatMessagesListData(chatViewStatesList);
+
+            }
+        });
+
+        binding.activityChatSendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!Objects.requireNonNull(binding.chatMessageEditText.getText()).toString().isEmpty()){
+                    String message = binding.chatMessageEditText.getText().toString();
+                    // TODO : envoyer tout ça à un usecase ou un repo
+
+
+                } else {
+                    Toast.makeText(
+                            ChatActivity.this,
+                            "Type your text first !",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/kardabel/go4lunch/ui/chat/ChatAdapter.java
+++ app/src/main/java/com/kardabel/go4lunch/ui/chat/ChatAdapter.java
@@ -1,11 +1,100 @@
 package com.kardabel.go4lunch.ui.chat;
 
-import com.kardabel.go4lunch.ui.workmates.WorkMatesViewState;
 
-public class ChatAdapter {
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.kardabel.go4lunch.R;
+import com.kardabel.go4lunch.model.ChatMessageModel;
+import com.kardabel.go4lunch.ui.restaurants.RestaurantsViewState;
+
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChatAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private final Context context;
+    List<ChatViewState> chatMessagesList;
+    public static final int MESSAGE_TYPE_IN = 1;
+   // public static final int MESSAGE_TYPE_OUT = 2;
+
+    public ChatAdapter(Context context) {
+        this.context = context;
+    }
+
+    // VIEW HOLDER FOR INCOMING MESSAGE
+    private class MessageInViewHolder extends RecyclerView.ViewHolder {
+
+        TextView message,date;
+
+        MessageInViewHolder(final View itemView) {
+            super(itemView);
+            message = itemView.findViewById(R.id.incoming_message_text);
+            date = itemView.findViewById(R.id.incoming_message_date);
+        }
+        void bind(int position) {
+            ChatViewState chatViewState = chatMessagesList.get(position);
+            message.setText(chatViewState.getChatMessageViewState());
+            date.setText(DateFormat.getTimeInstance(DateFormat.SHORT).format(chatViewState.getChatMessageTimeViewState()));
+        }
+    }
+
+    // VIEW HOLDER FOR OUTGOING MESSAGE
+    private class MessageOutViewHolder extends RecyclerView.ViewHolder {
+
+        TextView message,date;
+        MessageOutViewHolder(final View itemView) {
+            super(itemView);
+            message = itemView.findViewById(R.id.outgoing_message_text);
+            date = itemView.findViewById(R.id.outgoing_message_date);
+        }
+        void bind(int position) {
+            ChatViewState chatViewState = chatMessagesList.get(position);
+            message.setText(chatViewState.getChatMessageViewState());
+            date.setText(DateFormat.getTimeInstance(DateFormat.SHORT).format(chatViewState.getChatMessageTimeViewState()));
+        }
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (viewType == MESSAGE_TYPE_IN) {
+            return new MessageInViewHolder(LayoutInflater.from(context).inflate(R.layout.item_chat_incoming, parent, false));
+        }
+        return new MessageOutViewHolder(LayoutInflater.from(context).inflate(R.layout.item_chat_outgoing, parent, false));
+    }
 
 
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        if (chatMessagesList.get(position).getChatMessageTypeViewState() == MESSAGE_TYPE_IN) {
+            ((MessageInViewHolder) holder).bind(position);
+        } else {
+            ((MessageOutViewHolder) holder).bind(position);
+        }
+    }
 
+    @Override
+    public int getItemCount() {
+        return chatMessagesList.size();
+    }
 
+    @Override
+    public int getItemViewType(int position) {
+        return chatMessagesList.get(position).getChatMessageTypeViewState();
+    }
 
+    // CHAT MESSAGES LIST FROM ACTIVITY
+    @SuppressLint("NotifyDataSetChanged")
+    public void setChatMessagesListData(List<ChatViewState> chatMessagesList) {
+        this.chatMessagesList = chatMessagesList;
+        notifyDataSetChanged();
+
+    }
 }
