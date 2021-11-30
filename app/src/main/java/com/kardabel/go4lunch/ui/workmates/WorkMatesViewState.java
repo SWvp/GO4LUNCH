cb87dad532e20eb3d0e7ca05590880fc3cfab948
stephane warin <serkalidev@gmail.com>
2021-11-30 10:19:28 +0100

    in workmatesViewState, add a pram with the name, it will be retrieve in chat view

--- app/src/main/java/com/kardabel/go4lunch/ui/workmates/WorkMatesViewState.java
+++ app/src/main/java/com/kardabel/go4lunch/ui/workmates/WorkMatesViewState.java
@@ -4,19 +4,24 @@ import java.util.Objects;
 
 public class WorkMatesViewState {
 
+    private final String workmateName;
     private final String workmateDescription;
     private final String workmatePhoto;
     private final String workmateRestaurant;
     private final String workmateId;
     private boolean gotRestaurant;
 
-    public WorkMatesViewState(String workmateDescription, String workmatePhoto, String workmateRestaurant,String workmateId, boolean gotRestaurant) {
+    public WorkMatesViewState(String workmateName, String workmateDescription, String workmatePhoto, String workmateRestaurant,String workmateId, boolean gotRestaurant) {
+        this.workmateName = workmateName;
         this.workmateDescription = workmateDescription;
         this.workmatePhoto = workmatePhoto;
         this.workmateRestaurant = workmateRestaurant;
         this.gotRestaurant = gotRestaurant;
         this.workmateId = workmateId;
+
     }
+
+    public String getWorkmateName() { return workmateName; }
 
     public String getWorkmateDescription() {
         return workmateDescription;
@@ -36,27 +41,5 @@ public class WorkMatesViewState {
         return gotRestaurant;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        WorkMatesViewState that = (WorkMatesViewState) o;
-        return gotRestaurant == that.gotRestaurant && Objects.equals(workmateDescription, that.workmateDescription) && Objects.equals(workmatePhoto, that.workmatePhoto) && Objects.equals(workmateRestaurant, that.workmateRestaurant) && Objects.equals(workmateId, that.workmateId);
-    }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(workmateDescription, workmatePhoto, workmateRestaurant, workmateId, gotRestaurant);
-    }
-
-    @Override
-    public String toString() {
-        return "WorkMatesViewState{" +
-                "workmateDescription='" + workmateDescription + '\'' +
-                ", workmatePhoto='" + workmatePhoto + '\'' +
-                ", workmateRestaurant='" + workmateRestaurant + '\'' +
-                ", workmateId='" + workmateId + '\'' +
-                ", gotRestaurant=" + gotRestaurant +
-                '}';
-    }
 }
