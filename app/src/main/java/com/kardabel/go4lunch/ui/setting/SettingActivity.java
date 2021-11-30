fd0ec2eae9101418d548b86b291e98f327a6092c
stephane warin <serkalidev@gmail.com>
2021-11-30 15:10:58 +0100

    add SettingActivity

---
+++ app/src/main/java/com/kardabel/go4lunch/ui/setting/SettingActivity.java
@@ -0,0 +1,16 @@
+package com.kardabel.go4lunch.ui.setting;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
