86866a02ead3ec1d24e55993e4b158a696fea988
stephane warin <serkalidev@gmail.com>
2021-12-01 15:35:01 +0100

    add a remove function on the list during modified state of the firestore event to workmatesWhoMadesRestaurantChoice

--- app/src/main/java/com/kardabel/go4lunch/repository/WorkmatesWhoMadeRestaurantChoiceRepository.java
+++ app/src/main/java/com/kardabel/go4lunch/repository/WorkmatesWhoMadeRestaurantChoiceRepository.java
@@ -11,6 +11,7 @@ import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.kardabel.go4lunch.model.UserModel;
 import com.kardabel.go4lunch.model.WorkmateWhoMadeRestaurantChoice;
 
 import java.time.LocalDate;
@@ -27,37 +28,54 @@ public class WorkmatesWhoMadeRestaurantChoiceRepository {
     public LiveData<List<WorkmateWhoMadeRestaurantChoice>> getWorkmatesWhoMadeRestaurantChoice() {
         MutableLiveData<List<WorkmateWhoMadeRestaurantChoice>> userModelMutableLiveData = new MutableLiveData<>();
 
-        List<WorkmateWhoMadeRestaurantChoice> usersWithRestaurant = new ArrayList<>();
-
         LocalDate today = LocalDate.now();
+
+        List<WorkmateWhoMadeRestaurantChoice> usersWithRestaurant = new ArrayList<>();
 
         db.collection(today.toString())
                 .addSnapshotListener(new EventListener<QuerySnapshot>() {
                     @Override
                     public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
-
                         if (error != null) {
                             Log.e("restaurant choice error", error.getMessage());
                             return;
                         }
-                      //  List<WorkmateWhoMadeRestaurantChoice> userWithRestaurant = new ArrayList<>();
-
+                        //  List<WorkmateWhoMadeRestaurantChoice> userWithRestaurant = new ArrayList<>();
 
                         assert value != null;
                         for (DocumentChange document : value.getDocumentChanges()) {
+                            Log.d("pipo", "onEvent() called with: value = [" + document.getDocument().toObject(WorkmateWhoMadeRestaurantChoice.class) + "], error = [" + error + "]");
                             if (document.getType() == DocumentChange.Type.ADDED ) {
 
                                 usersWithRestaurant.add(document.getDocument().toObject(WorkmateWhoMadeRestaurantChoice.class));
 
-                            }else if(document.getType() == DocumentChange.Type.REMOVED){
+                            }
+
+                            else if (document.getType() == DocumentChange.Type.MODIFIED ) {
+
+                                for (int i = 0; i < usersWithRestaurant.size(); i++) {
+                                    if(usersWithRestaurant.get(i).getUserId().equals(document.getDocument().toObject(WorkmateWhoMadeRestaurantChoice.class).getUserId())){
+                                        usersWithRestaurant.remove(usersWithRestaurant.get(i));
+                                    }
+
+                                }
+
+                                usersWithRestaurant.add(document.getDocument().toObject(WorkmateWhoMadeRestaurantChoice.class));
+
+                            }
+
+                            else if(document.getType() == DocumentChange.Type.REMOVED){
 
 
                                 usersWithRestaurant.remove(document.getDocument().toObject(WorkmateWhoMadeRestaurantChoice.class));
 
                             }
+
+                           // TODO: SWITCH CASE
                         }
-                        List<WorkmateWhoMadeRestaurantChoice> list = new ArrayList<>(usersWithRestaurant);
-                        userModelMutableLiveData.setValue(list);
+
+                       // List<WorkmateWhoMadeRestaurantChoice> usersWhoChose = new ArrayList<>(usersWithRestaurant);
+                        userModelMutableLiveData.setValue(usersWithRestaurant);
                     }
                 });
         return userModelMutableLiveData;
