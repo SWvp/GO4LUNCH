24f641f38747678f27bfbdfa00eefa55a0f63c4c
stephane warin <serkalidev@gmail.com>
2021-12-03 12:04:04 +0100

    add distance with int type to sort the list in VM

--- app/src/main/java/com/kardabel/go4lunch/ui/restaurants/RestaurantsViewState.java
+++ app/src/main/java/com/kardabel/go4lunch/ui/restaurants/RestaurantsViewState.java
@@ -1,28 +1,37 @@
 package com.kardabel.go4lunch.ui.restaurants;
 
+import androidx.annotation.ColorRes;
+
 import java.util.Objects;
 
 public class RestaurantsViewState {
 
+    private final int distanceInt;
     private final String name;
     private final String address;
     private final String photo;
-    private final String distance;
+    private final String distanceText;
     private final String openingHours;
     private final double rating;
     private final String placeId;
     private final String usersWhoChoseThisRestaurant;
+    @ColorRes
+    private final int textColor;
 
-    public RestaurantsViewState(String name, String address, String photo, String distance, String openingHours, double rating, String placeId, String usersWhoChoseThisRestaurant) {
+    public RestaurantsViewState(int distanceInt, String name, String address, String photo, String distanceText, String openingHours, double rating, String placeId, String usersWhoChoseThisRestaurant, int textColor) {
+        this.distanceInt = distanceInt;
         this.name = name;
         this.address = address;
         this.photo = photo;
-        this.distance = distance;
+        this.distanceText = distanceText;
         this.openingHours = openingHours;
         this.rating = rating;
         this.placeId = placeId;
         this.usersWhoChoseThisRestaurant = usersWhoChoseThisRestaurant;
+        this.textColor = textColor;
     }
+
+    public int getDistanceInt() { return distanceInt; }
 
     public String getName() { return name; }
 
@@ -30,7 +39,7 @@ public class RestaurantsViewState {
 
     public String getPhoto() { return photo; }
 
-    public String getDistance() { return distance; }
+    public String getDistanceText() { return distanceText; }
 
     public String getOpeningHours() { return openingHours; }
 
@@ -40,30 +49,34 @@ public class RestaurantsViewState {
 
     public String getUsersWhoChoseThisRestaurant() { return usersWhoChoseThisRestaurant; }
 
+    public int getTextColor() { return textColor; }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         RestaurantsViewState that = (RestaurantsViewState) o;
-        return Double.compare(that.rating, rating) == 0 && Objects.equals(name, that.name) && Objects.equals(address, that.address) && Objects.equals(photo, that.photo) && Objects.equals(distance, that.distance) && Objects.equals(openingHours, that.openingHours) && Objects.equals(placeId, that.placeId) && Objects.equals(usersWhoChoseThisRestaurant, that.usersWhoChoseThisRestaurant);
+        return distanceInt == that.distanceInt && Double.compare(that.rating, rating) == 0 && textColor == that.textColor && Objects.equals(name, that.name) && Objects.equals(address, that.address) && Objects.equals(photo, that.photo) && Objects.equals(distanceText, that.distanceText) && Objects.equals(openingHours, that.openingHours) && Objects.equals(placeId, that.placeId) && Objects.equals(usersWhoChoseThisRestaurant, that.usersWhoChoseThisRestaurant);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, address, photo, distance, openingHours, rating, placeId, usersWhoChoseThisRestaurant);
+        return Objects.hash(distanceInt, name, address, photo, distanceText, openingHours, rating, placeId, usersWhoChoseThisRestaurant, textColor);
     }
 
     @Override
     public String toString() {
         return "RestaurantsViewState{" +
-                "name='" + name + '\'' +
+                "distanceInt=" + distanceInt +
+                ", name='" + name + '\'' +
                 ", address='" + address + '\'' +
                 ", photo='" + photo + '\'' +
-                ", distance='" + distance + '\'' +
+                ", distanceText='" + distanceText + '\'' +
                 ", openingHours='" + openingHours + '\'' +
                 ", rating=" + rating +
                 ", placeId='" + placeId + '\'' +
                 ", usersWhoChoseThisRestaurant='" + usersWhoChoseThisRestaurant + '\'' +
+                ", textColor=" + textColor +
                 '}';
     }
 }
