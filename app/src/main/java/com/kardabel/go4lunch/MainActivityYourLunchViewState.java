74c45b8e020b536835e985f886893dfd77c6fc9f
stephane warin <serkalidev@gmail.com>
2021-12-06 16:20:05 +0100

    create view state for 'your_lunch'

---
+++ app/src/main/java/com/kardabel/go4lunch/MainActivityYourLunchViewState.java
@@ -0,0 +1,43 @@
+package com.kardabel.go4lunch;
+
+import java.util.Objects;
+
+public class MainActivityYourLunchViewState {
+
+    private final String restaurantId;
+    private final int currentUserRestaurantChoiceStatus;
+
+    public MainActivityYourLunchViewState(String restaurantId, int currentUserRestaurantChoiceStatus) {
+        this.restaurantId = restaurantId;
+        this.currentUserRestaurantChoiceStatus = currentUserRestaurantChoiceStatus;
+    }
+
+    public String getRestaurantId() {
+        return restaurantId;
+    }
+
+    public int getCurrentUserRestaurantChoiceStatus() {
+        return currentUserRestaurantChoiceStatus;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MainActivityYourLunchViewState that = (MainActivityYourLunchViewState) o;
+        return currentUserRestaurantChoiceStatus == that.currentUserRestaurantChoiceStatus && Objects.equals(restaurantId, that.restaurantId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(restaurantId, currentUserRestaurantChoiceStatus);
+    }
+
+    @Override
+    public String toString() {
+        return "MainActivityYourLunchViewState{" +
+                "restaurantId='" + restaurantId + '\'' +
+                ", currentUserRestaurantChoiceStatus=" + currentUserRestaurantChoiceStatus +
+                '}';
+    }
+}
