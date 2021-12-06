62f5664d01e232ada50bc78c416c55c9188b08ae
stephane warin <serkalidev@gmail.com>
2021-12-06 16:13:55 +0100

    Add compare method in workMatesVM, to sort by user who have chosen or not

--- app/src/main/java/com/kardabel/go4lunch/ui/workmates/WorkMatesViewModel.java
+++ app/src/main/java/com/kardabel/go4lunch/ui/workmates/WorkMatesViewModel.java
@@ -10,11 +10,13 @@ import androidx.lifecycle.ViewModel;
 
 import com.kardabel.go4lunch.R;
 import com.kardabel.go4lunch.model.UserModel;
-import com.kardabel.go4lunch.model.WorkmateWhoMadeRestaurantChoice;
+import com.kardabel.go4lunch.model.UserWhoMadeRestaurantChoice;
 import com.kardabel.go4lunch.repository.WorkmatesRepository;
-import com.kardabel.go4lunch.repository.WorkmatesWhoMadeRestaurantChoiceRepository;
+import com.kardabel.go4lunch.repository.UsersWhoMadeRestaurantChoiceRepository;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class WorkMatesViewModel extends ViewModel {
@@ -26,7 +28,7 @@ public class WorkMatesViewModel extends ViewModel {
     public WorkMatesViewModel(
             @NonNull Application application,
             @NonNull WorkmatesRepository workmatesRepository,
-            @NonNull WorkmatesWhoMadeRestaurantChoiceRepository workmatesWhoMadeRestaurantChoiceRepository
+            @NonNull UsersWhoMadeRestaurantChoiceRepository usersWhoMadeRestaurantChoiceRepository
     ) {
 
         this.application = application;
@@ -36,8 +38,8 @@ public class WorkMatesViewModel extends ViewModel {
         // AND ONE WITH USERS (WORKMATES) WHO MADE A CHOICE
         LiveData<List<UserModel>> workMatesLiveData =
                 workmatesRepository.getWorkmates();
-        LiveData<List<WorkmateWhoMadeRestaurantChoice>> workmatesWhoMadeChoiceLiveData =
-                workmatesWhoMadeRestaurantChoiceRepository.getWorkmatesWhoMadeRestaurantChoice();
+        LiveData<List<UserWhoMadeRestaurantChoice>> workmatesWhoMadeChoiceLiveData =
+                usersWhoMadeRestaurantChoiceRepository.getWorkmatesWhoMadeRestaurantChoice();
 
         // OBSERVERS
         workMatesViewStateMediatorLiveData.addSource(workMatesLiveData, workmates ->
@@ -54,7 +56,7 @@ public class WorkMatesViewModel extends ViewModel {
     // COMBINE THE 2 SOURCES
     private void combine(
             List<UserModel> workmates,
-            List<WorkmateWhoMadeRestaurantChoice> workmatesWhoMadeRestaurantChoices) {
+            List<UserWhoMadeRestaurantChoice> workmatesWhoMadeRestaurantChoices) {
 
         if (workmatesWhoMadeRestaurantChoices != null) {
             workMatesViewStateMediatorLiveData.setValue(mapWorkmates(
@@ -66,11 +68,10 @@ public class WorkMatesViewModel extends ViewModel {
 
     // MAP TO WORKMATE VIEW STATE
     private List<WorkMateViewState> mapWorkmates(List<UserModel> workmates,
-                                                 List<WorkmateWhoMadeRestaurantChoice> workmatesWhoMadeRestaurantChoices) {
+                                                 List<UserWhoMadeRestaurantChoice> workmatesWhoMadeRestaurantChoices) {
         List<WorkMateViewState> workMateViewStateList = new ArrayList<>();
 
         for (int i = 0; i < workmates.size(); i++) {
-
 
             String workmateName = workmates.get(i).getUserName();
             String avatar = workmates.get(i).getAvatarURL();
@@ -80,8 +81,6 @@ public class WorkMatesViewModel extends ViewModel {
             int colorText = Color.GRAY;
             if(isUserHasDecided(workmateId, workmatesWhoMadeRestaurantChoices)){
                 colorText = Color.BLACK;
-
-
             }
 
             workMateViewStateList.add(new WorkMateViewState(
@@ -94,13 +93,21 @@ public class WorkMatesViewModel extends ViewModel {
 
             ));
         }
+
+        // SORT THE LIST BY BOOLEAN, IF TRUE, APPEARS AT THE TOP OF THE LIST
+        Collections.sort(workMateViewStateList, new Comparator<WorkMateViewState>() {
+            @Override
+            public int compare(WorkMateViewState o1, WorkMateViewState o2) {
+                return Boolean.compare(!o1.isUserHasDecided(), !o2.isUserHasDecided());
+            }
+        });
         return workMateViewStateList;
 
     }
 
     // GET THE LUNCH CHOICE STATUS OF EVERY WORKMATES
     private boolean isUserHasDecided(String workmateId,
-                                     List<WorkmateWhoMadeRestaurantChoice> workmatesWhoMadeRestaurantChoices) {
+                                     List<UserWhoMadeRestaurantChoice> workmatesWhoMadeRestaurantChoices) {
         boolean state = false;
 
         for (int i = 0; i < workmatesWhoMadeRestaurantChoices.size(); i++) {
@@ -115,7 +122,7 @@ public class WorkMatesViewModel extends ViewModel {
 
     // GET THE RESTAURANT WHO HAS BEEN CHOSEN
     private String workmateChoice(@NonNull String workmateId,
-                                  @NonNull List<WorkmateWhoMadeRestaurantChoice> workmatesWhoMadeRestaurantChoices) {
+                                  @NonNull List<UserWhoMadeRestaurantChoice> workmatesWhoMadeRestaurantChoices) {
         String restaurantName = " " + application.getString(R.string.not_decided);
 
         for (int i = 0; i < workmatesWhoMadeRestaurantChoices.size(); i++) {
